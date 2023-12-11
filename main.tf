de40093ca7797aa09da5b3081cc9daf695da6ce8
bency <bency@123>
2023-12-11 10:50:02 +0000

    webserver SG created

--- main.tf
+++ main.tf
@@ -9,3 +9,40 @@ resource "aws_key_pair" "frontend" {
     Name = "${var.project_name}-${var.project_env}"
   }
 }
+#----------------------------------------------------------------------------
+#                      webserver SecurityGroup
+#----------------------------------------------------------------------------
+resource "aws_security_group" "webserver_access" {
+  name        = "${var.project_name}-${var.project_env}-webserver-access"
+  description = "${var.project_name}-${var.project_env}-webserver-access"
+  ingress {
+    description      = "http traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "https traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-weberver-access"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
